c82b101eba907f5d9c1c9d4e435ed094ef2833cd
Adriano Mota <adrianowsh@gmail.com>
2025-12-14 20:02:12 -0300

    feat: finish Todoitem entity domain

--- src/Todofy.Domain/Todos/TodoItem.cs
+++ src/Todofy.Domain/Todos/TodoItem.cs
@@ -1,7 +1,57 @@
-﻿namespace Todofy.Domain.Todos;
+﻿using Todofy.SharedKernel;
 
+namespace Todofy.Domain.Todos;
 
-public sealed class TodoItem
+public sealed class TodoItem : Entity
 {
+    private TodoItem(
+        string id,
+        string description,
+        DateTime? dueDate,
+        bool isComplete, DateTime? completedAt, Priority priority)
+    {
+        Id = id;
+        Description = description;
+        DueDate = dueDate;
+        IsComplete = isComplete;
+        CompletedAt = completedAt;
+        Priority = priority;
+    }
+
+    private TodoItem() { }
+
+    public string Id { get; private set; }
     public string Description { get; private set; }
+    public DateTime? DueDate { get; private set; }
+    public bool IsComplete { get; private set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public Priority Priority { get; private set; }
+
+    public DateTime? CompletedAt { get; private set; }
+    public static TodoItem Create(
+        string description,
+        DateTime? dueDate,
+        bool isComplete,
+        DateTime? completedAt,
+        Priority priority)
+    {
+        return new TodoItem(
+            id: $"tdo_{Guid.CreateVersion7()}",
+            description,
+            dueDate,
+            isComplete,
+            completedAt,
+            priority);
+    }
+
+    public void CompleteItem()
+    {
+        IsComplete = true;
+        CompletedAt = DateTime.UtcNow;
+    }
+
+    public void SetDueDate(DateTime? dueDate)
+    {
+        DueDate = dueDate;
+    }
 }
